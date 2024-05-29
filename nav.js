ce6b1ee61f4992af6c7e80a7633874fc6e84107b
OscarAIH <158465664+OscarAIH@users.noreply.github.com>
2024-05-29 13:47:15 +0200

    alt muligt

---
+++ nav.js
@@ -0,0 +1,8 @@
+document.getElementById('burger-menu').addEventListener('click', function() {
+    var navMenu = document.getElementById('nav-menu');
+    if (navMenu.classList.contains('show')) {
+        navMenu.classList.remove('show');
+    } else {
+        navMenu.classList.add('show');
+    }
+});
